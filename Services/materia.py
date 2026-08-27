823d8174d40044400c19470a886280c173174404
MauricioM <morenoacosta802@gmail.com>
2026-08-27 16:40:06 -0500

    adelanto 2 servicios

---
+++ Services/materia.py
@@ -0,0 +1,18 @@
+from flask import current_app
+
+
+class materiaService:
+    
+    
+ def add():
+        pass
+    
+def update():
+        pass
+    
+def delete():
+        pass
+        
+        
+def read():
+      pass
